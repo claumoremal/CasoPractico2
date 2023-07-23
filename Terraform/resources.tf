bf9ec720e8906c64d73832a3b1d35af7d637fe73
claudio <claumoremal@gmail.com>
2023-07-22 22:06:00 -0300

    se agrega el acr a resources.tf

--- Terraform/resources.tf
+++ Terraform/resources.tf
@@ -52,4 +52,10 @@ resource "azurerm_network_interface" "vnic" {
 }
 
 //definicion de container registry con elservicio de azure acr
-
+resource "azurerm_container_registry" "acr" {
+  name                = "acr-casoPractico"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
