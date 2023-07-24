09d922f6275a833576ef3d65ccf0313273ce25d2
claudio <claumoremal@gmail.com>
2023-07-24 14:53:47 -0300

    se define otra regla en el nsg para permitir el trafico desde internet a la webapp de podman en el puerto 8080

--- Terraform/resources.tf
+++ Terraform/resources.tf
@@ -117,7 +117,7 @@ resource "azurerm_network_security_group" "nsgPodman" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "80"
+    destination_port_range     = "8080"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
